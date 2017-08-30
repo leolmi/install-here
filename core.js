bf0cf94adb242bf0f518935587899e3a80fb09aa
leolmi <leo.olmi@gmail.com>
2017-08-30 09:00:21 +0200

    fix

--- core.js
+++ core.js
@@ -450,29 +450,32 @@ function _checkPath(f) {
 function _replacePkgFile(f) {
   var nf = f.replace(_state.relpath + path.sep, '');
   if (_state.settings._filters.ignore.check(nf))
-    return _log('Skip file: '+nf);
+    return _log('Skip file: ' + nf);
 
   _checkPath(nf);
 
   var xdata = null;
-  var ispkj = (path.basename(nf)==_constants.PACKAGE_CONFIG);
+  var ispkj = (path.basename(nf) == _constants.PACKAGE_CONFIG);
   if (ispkj && _state.options.patch)
-    return _log('Skip overwriting file: '+nf);
+    return _log('Skip overwriting file: ' + nf);
   if (fs.existsSync(nf)) {
     if (_state.settings._filters.ignoreOverwrite.check(nf))
-      return _log('Skip overwriting file: '+nf);
+      return _log('Skip overwriting file: ' + nf);
     if (ispkj && !_state.options.skipkg) xdata = fs.readFileSync(nf);
     //elimina il file originale
     fs.unlinkSync(nf);
   }
-  _log('replace file: ' + nf);
   //legge il file
   var data = fs.readFileSync(f);
-  if (ispkj && xdata) {
-    data = _state.managePkg(xdata, data);
-  } else if (_.isFunction(_state.manageFile)) {
-    data = _state.manageFile(data, nf);
+  if (xdata) {
+    if (ispkj) {
+      data = _state.managePkg(xdata, data);
+    } else if (_.isFunction(_state.manageFile)) {
+      data = _state.manageFile(data, nf);
+    }
   }
+  if (!data) return _log('Skip file: ' + nf);
+  _log('replace file: ' + nf);
   //scrive il nuovo file
   fs.writeFileSync(nf, data);
   _state.counters.files++;
